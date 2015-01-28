1e5bc1fc7e28604a6c6b447755e55a96c04cbccf
mrMams <mrMams@users.norepy.github.com>
2015-01-28 22:51:25 +0100

    coding the solution for Project Euler's Pb #3

---
+++ projecteuler/primefactors.py
@@ -0,0 +1,18 @@
+
+
+'''
+candidats = liste des nombres de 2 a n
+prime = 2
+primes = [2]
+tant que candidats non vide
+	eliminer tous les nombres divisibles par k de candidats
+	prime = le plus petit element de candidats
+	primes.add(prime)
+	candidats.remove(prime)
+retourner le plus grand element de candidats
+'''
+
+def largestPrimeFactor(n):
+	return result
+
+
