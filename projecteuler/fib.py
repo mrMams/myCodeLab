58f198753fd77c453ca44710e03881819e7b6a37
mrMams <mrMams@users.norepy.github.com>
2015-01-28 22:13:13 +0100

    Fixed in order to solve Project Euler's Problem #2

--- projecteuler/fib.py
+++ projecteuler/fib.py
@@ -15,15 +15,16 @@ for i in range(1, number+1):
         print "fib(%d) = %d %s" % (i, fibi, ("pair" if fibi%2 == 0 else ""))
 
 # TODO:
-# - Algo: integrer cette fonction avec la fonction fib ci-dessus pour memoriser les calculs intermediaires de fib
 # - Python: utiliser un generateur
-def sumPairFib():
+def sumPairFib(n):
         k, currentFib = 2, fib(2)
         result = currentFib
-        while currentFib < 4000000:
-                k, currentFib = (k+3), fib(k)
+        print "k=%d, currentFib=%d, result=%d" % (k, currentFib, result)
+        while True:
+                k, currentFib = (k+3), fib(k+3)
+		if currentFib >=  n: break
                 result += currentFib
                 print "k=%d, currentFib=%d, result=%d" % (k, currentFib, result)
         return result
 
-print "La somme des termes voulus est: %d" % sumPairFib()
+print "La somme des termes voulus est: %d" % sumPairFib(4000000)
