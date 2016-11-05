9e816b81606ca820847df1309c57d941073ce236
mrMams <mrMams@users.norepy.github.com>
2016-11-05 01:42:47 +0100

    Solved Project Euler problem #5

--- projecteuler/problem5.py
+++ projecteuler/problem5.py
@@ -2,25 +2,17 @@
 
 import sys
 
-# TODO: check only prime numbers instead of all integers
-def isDivisibleByAllIntegersBelow(nb, maxNb):
-	for i in xrange(2, maxNb+1):
-		if nb % i != 0:
-			return False
-	return True
-
-# Testing isDivisibleByAllIntegersBelow
-print "Is %d divisible by all numbers from 1 to %d? %s" % (2520, 10, isDivisibleByAllIntegersBelow(2520, 10))
-print "Is %d divisible by all numbers from 1 to %d? %s" % (2520, 11, isDivisibleByAllIntegersBelow(2520, 11))
-
-
 def getSmallestMultiple(maxNb):
 	if maxNb <= 2:
 		return maxNb
 	candidateNb = getSmallestMultiple(maxNb - 1)
-	while not isDivisibleByAllIntegersBelow(candidateNb, maxNb):
-		candidateNb += 1
-	return candidateNb
+	if candidateNb % maxNb == 0:
+		return candidateNb
+	else:
+		i = 2
+		while (candidateNb * i) % maxNb != 0:
+			i += 1
+		return candidateNb *i
 
 maxNb = int(sys.argv[1])
 print "Smallest positive nb divisible by all numbers from 1 to %d is: %d" % (maxNb,getSmallestMultiple(maxNb))
